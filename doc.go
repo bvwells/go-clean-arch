c16ec718f311cc7dfe05135b8fc47a7d0aab3883
Ben Wells <b.v.wells@gmail.com>
2017-11-30 22:30:25 +0000

    Fix godoc

--- doc.go
+++ doc.go
@@ -1,17 +1,17 @@
 /*
-  Go-clean-arch is a linter for enforcing clean architecture principles in Go.
+Go-clean-arch is a linter for enforcing clean architecture principles in Go.
 
-  Usage:
-  	go-clean-arch [flags] [path ...]
+Usage:
+  go-clean-arch [flags] [path ...]
 
   The flags are:
     -c  Config file containing list of clean architecture layers from
         inner layers to outer laters.
 
-  Examples
+Examples
 
   To check go source code folder containing clean architecture layers:
 
-  	go-clean-arch -c config.cfg path
+    go-clean-arch -c config.cfg path
 */
 package main
