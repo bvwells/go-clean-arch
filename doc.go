94c16b4adea8e85e3df9e30546428d2d35d7b07b
Ben Wells <b.v.wells@gmail.com>
2017-12-03 19:43:55 +0000

    Add more details to doc

--- doc.go
+++ doc.go
@@ -1,6 +1,9 @@
 /*
 Go-clean-arch is a linter for enforcing clean architecture principles in Go.
 
+The linter reports violations of clean architecture dependency rules by checking go imports 
+within packages against user defined dependency rules.
+
 Usage:
   go-clean-arch [flags] [path]
 
@@ -13,5 +16,23 @@ Examples
   To check go source code folder containing clean architecture layers:
 
     go-clean-arch -c config.json path
+    
+  The go-clean-arch linter can be run on the Git repo https://github.com/ManuelKiessling/go-cleanarchitecture by cloning the repo using the command;
+
+   git clone https://github.com/ManuelKiessling/go-cleanarchitecture
+
+  Run the linter with the command:
+
+    go-clean-arch -c layers.json path-to-repo\go-cleanarchitecture\src
+
+  where the layers config file contains the clean architecture layers:
+
+    {
+      "domain": 1,
+      "usecases": 2,
+      "interfaces": 3,
+      "infrastructure": 4
+    }
+
 */
 package main
